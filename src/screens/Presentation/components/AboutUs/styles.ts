71efa7e146dc1bfb81270d6de3927171be7bab65
DanielSantos01 <danielspovv@gmail.com>
2021-12-06 17:01:20 -0300

    feat: team section on the presentation screen

--- src/screens/Presentation/components/AboutUs/styles.ts
+++ src/screens/Presentation/components/AboutUs/styles.ts
@@ -7,19 +7,40 @@ export const Container = styled.div`
   width: 100%;
   padding: 30px 70px;
   background-color: ${({ theme }) => theme.colors.terciary};
+  box-shadow: 0 2px 10px -8px rgba(0, 0, 0, 0.8);
+  padding-bottom: 5px;
 `;
 
 export const Title = styled.p`
+  font-family: ${({ theme }) => theme.fonts.primary};
   font-size: 2vw;
   color: black;
   text-align: center;
   font-weight: 600;
+
+  @media(max-width: 770px) {
+    font-size: 3vw;
+  }
+
+  @media(max-width: 500px) {
+    font-size: 5vw;
+  }
 `;
 
 export const ContentContainer = styled.div`
   display: flex;
   height: 100%;
   margin-top: 10vh;
+
+  @media(max-width: 1470px) {
+    margin-top: 0vh;
+    align-items: center;
+  }
+
+  @media(max-width: 770px) {
+    flex-direction: column;
+    align-items: fles-start;
+  }
 `;
 
 export const ImageContainer = styled.div`
@@ -27,11 +48,20 @@ export const ImageContainer = styled.div`
   height: 50%;
   display: flex;
   justify-content: center;
+
+  @media(max-width: 770px) {
+    height: auto;
+    align-items: flex-end;
+  }
 `;
 
 export const Image = styled.img`
   height: 100%;
   width: 70%;
+
+  @media(max-width: 770px) {
+    height: 50%;
+  }
 `;
 
 export const TextContainer = styled.div`
@@ -44,6 +74,19 @@ export const TextContainer = styled.div`
 `;
 
 export const Text = styled.p`
+  font-family: ${({ theme }) => theme.fonts.primary};
   margin-bottom: 30px;
-  font-size: 23px;
+  font-size: 1.2em;
+
+  @media(max-width: 1470px) {
+    font-size: 1em;
+  }
+
+  @media(max-width: 770px) {
+    font-size: 0.8em;
+  }
+
+  @media(max-width: 500px) {
+    font-size: 0.6em;
+  }
 `;
---
+++ src/screens/Presentation/components/Team/data/team.ts
@@ -0,0 +1,30 @@
+export const team = [
+  {
+    name: 'Alexandre',
+    picture: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+  },
+  {
+    name: 'Daniel',
+    picture: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+  },
+  {
+    name: 'Felipe',
+    picture: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+  },
+  {
+    name: 'Sara',
+    picture: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+  },
+  {
+    name: 'Edna',
+    picture: 'http://s2.glbimg.com/FmBS8r6jCZNjGQ7dIRNooMSu-eH6d2h4XlhUeNEf3eVIoz-HdGixxa_8qOZvMp3w/s.glbimg.com/og/rg/f/original/2012/07/27/edna_291_388.jpg',
+  },
+  {
+    name: 'Vinícius',
+    picture: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+  },
+  {
+    name: 'Gabriel',
+    picture: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+  },
+];
